24a747a4b428d2f86798fb64ad2149b9d7b6acad
Itay Finkelshtein <itayf9@gmail.com>
2022-12-27 17:35:10 +0200

    Add connection to Google Calendar API

---
+++ src/test/java/com/example/lamivhan/LamivhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lamivhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamivhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
