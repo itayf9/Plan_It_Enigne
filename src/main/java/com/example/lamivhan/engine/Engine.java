bf8071242bc8bfde213aafcccf80f8f6b06a4df3
Oshri Fadlon <oshrifa@mta.ac.il>
2023-02-10 23:22:15 +0200

    Changes by itayf add Utility

--- src/main/java/com/example/lamivhan/engine/Engine.java
+++ src/main/java/com/example/lamivhan/engine/Engine.java
@@ -3,7 +3,9 @@ package com.example.lamivhan.engine;
 import com.example.lamivhan.model.timeslot.TimeSlot;
 import com.example.lamivhan.model.user.User;
 import com.example.lamivhan.utill.Constants;
+import com.example.lamivhan.utill.Utility;
 import com.example.lamivhan.utill.dto.DTOfreetime;
+import com.example.lamivhan.utill.dto.DTOstartAndEndOfInterval;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
 
@@ -50,17 +52,68 @@ public class Engine {
         // gets user's preferences
         int userStudyStartTime = user.getUserPreferences().getUserStudyStartTime();
         int userStudyEndTime = user.getUserPreferences().getUserStudyEndTime();
-        int STUDY_SESSION_TIME = user.getUserPreferences().getStudySessionTime();
+        int userStudySessionTime = user.getUserPreferences().getStudySessionTime();
 
         // goes through the raw time slots
-        for (int i = 0; i < userFreeTimeSlots.size(); i++) {
+        for (TimeSlot userFreeTimeSlot : userFreeTimeSlots) {
 
-            // gets current raw slot end and start in millis
-            long startOfCurrentSlot = userFreeTimeSlots.get(i).getStart().getValue();
-            long endOfCurrentSlot = userFreeTimeSlots.get(i).getEnd().getValue();
+            // gets current slot end and start
+            Instant startOfCurrentSlot = Instant.ofEpochMilli(userFreeTimeSlot.getStart().getValue());
+            Instant endOfCurrentSlot = Instant.ofEpochMilli(userFreeTimeSlot.getEnd().getValue());
 
+            // finds the first place of user study start time (e.g. first 8:00)
+            // finds the first place of user study end time (e.g. first 22:00)
+            DTOstartAndEndOfInterval currentInterval = Utility.getCurrentInterval(startOfCurrentSlot, userStudyStartTime, userStudyEndTime);
+            Instant userStudyStartFirst = currentInterval.getStartOfInterval();
+            Instant userStudyEndFirst = currentInterval.getEndOfInterval();
 
-            // creates two "Instant"s that represent the day of the startOfCurrentSlot with different hours
+            Instant selectedStart;
+            Instant selectedEnd;
+
+            // we take the min(userStudyEndFirst,endOfCurrentSlot)
+            if (userStudyEndFirst.isAfter(endOfCurrentSlot)) { // the first 22:00 is after the end of slot
+                selectedEnd = endOfCurrentSlot;
+            } else {
+                selectedEnd = userStudyEndFirst;
+            }
+
+            // we take the max(userStudyStartFirst, startOfCurrentSlot)
+            if (userStudyStartFirst.isBefore(startOfCurrentSlot)) { // the first 08:00 is before the start of slot
+                selectedStart = startOfCurrentSlot;
+            } else {
+                selectedStart = userStudyStartFirst;
+            }
+
+            if (selectedStart.until(selectedEnd, ChronoUnit.HOURS) >= userStudySessionTime) {
+                // adds the study time of the first day
+                adjustedUserFreeSlots.add(new TimeSlot(new DateTime(selectedStart.toEpochMilli()), new DateTime(selectedEnd.toEpochMilli())));
+                totalFreeTime += (selectedEnd.toEpochMilli() - selectedStart.toEpochMilli()) / Constants.MILLIS_TO_HOUR;
+            }
+
+            // finds the next place of user study start time (e.g. next 8:00)
+            // finds the next place of user study end time (e.g. next 22:00)
+            Instant userStudyStartNext = Utility.addDayToCurrentInstant(userStudyStartFirst);
+            Instant userStudyEndNext = Utility.addDayToCurrentInstant(userStudyEndFirst);
+
+            while (userStudyEndNext.isBefore(endOfCurrentSlot)) {
+                // adds a full day study for the next day
+                adjustedUserFreeSlots.add(new TimeSlot(new DateTime(userStudyStartNext.toEpochMilli()), new DateTime(userStudyEndNext.toEpochMilli())));
+                totalFreeTime += (userStudyEndNext.toEpochMilli() - userStudyStartNext.toEpochMilli()) / Constants.MILLIS_TO_HOUR;
+
+                // finds the next place of user study start time (e.g. next 8:00)
+                // finds the next place of user study end time (e.g. next 22:00)
+                userStudyStartNext = Utility.addDayToCurrentInstant(userStudyStartFirst);
+                userStudyEndNext = Utility.addDayToCurrentInstant(userStudyEndFirst);
+            }
+
+            if ((userStudyStartNext.isBefore(endOfCurrentSlot))
+                    && (userStudyStartNext.until(endOfCurrentSlot, ChronoUnit.HOURS) >= userStudySessionTime)) {
+                // adds the study time of the last day
+                adjustedUserFreeSlots.add(new TimeSlot(new DateTime(userStudyStartNext.toEpochMilli()), new DateTime(endOfCurrentSlot.toEpochMilli())));
+                totalFreeTime += (endOfCurrentSlot.toEpochMilli() - userStudyStartNext.toEpochMilli()) / Constants.MILLIS_TO_HOUR;
+            }
+
+            /*// creates two "Instant"s that represent the day of the startOfCurrentSlot with different hours
             // an Instant with user's study start time (e.g. instance of 08:00 at the same day)
             // an Instant with user's study end time (e.g. instance of 22:00 at same day)
             Instant insOfUserStudyStartTime = Instant.ofEpochMilli(startOfCurrentSlot);
@@ -78,7 +131,7 @@ public class Engine {
                 // nothing to adjust here
                 // adds the slot to the list
                 adjustedUserFreeSlots.add(new TimeSlot(new DateTime(startOfCurrentSlot), new DateTime(endOfCurrentSlot)));
-                totalFreeTime += (endOfCurrentSlot - startOfCurrentSlot) / Constants.MILLIS_TO_HOUR;
+                totalFreeTime += (endOfCurrentSlot - startOfCurrentSlot) / Constants.MILLIS_TO_HOUR;*/
 
             /*
 
@@ -108,9 +161,8 @@ public class Engine {
 
                       v|---------START----X-----Y-----END----------| // first case
                       v|-----X---START------Y---------END----------| // second case
-                      v|---------START------X---------END----Y-----| else case
-                      |---X-----START----------------END---Y--23:59----| // need to complete, now it adds the slot twice (thinks it's both the first interval and second interval)
-                      v|---------START----------------END----------|
+                      v|---------START------X---------END-Y--23:59-| else case
+                       |--X------START----------------END-Y--23:59-| // need to complete, now it adds the slot twice (thinks it's both the first interval and second interval)
 
                        if (Y< END && X > START )
                        else if (Y< END && X <= START)
@@ -133,11 +185,58 @@ public class Engine {
 
 
 
+
+
+
+                     |---------------------X-------------------------Y--------------------|
+
+
+
+                     START = find first place of START
+                     END = find first place of END
+
+
+                     if END > Y // the first 22:00 is after the end of slot  // we take the min(END,Y)
+                        RealEND = Y
+                     else
+                        RealEND = END
+
+                     if START < X // the first 08:00 is before the start of slot  // we take the max(START,X)
+                        RealSTART = X
+                     else
+                        RealSTART = START
+
+                     if RealEnd - RealStart >= SESSION_TIME
+                        take (RealStart, RealEnd)
+
+                     STARTnext = find next place of START
+                     ENDnext = find next place of END
+
+                     while (STARTnext > END && ENDnext < Y) {
+                        take (STARTnext, ENDnext)
+                        END = ENDnext
+                        STARTnext = find next place of START
+                        ENDnext = find next place of END
+                        }
+
+
+                     if STARTnext < Y && Y-STARTnext >= SESSION_TIME
+                         take (STARTnext, Y)
+
+
+
+
+
+
+
+
+
+
                        // check if the order of the conditions in the ifs is good
 
              */
 
-            } else if (startOfCurrentSlot < insOfUserStudyStartTime.toEpochMilli()
+            /*} else if (startOfCurrentSlot < insOfUserStudyStartTime.toEpochMilli()
                     && endOfCurrentSlot <= insOfUserStudyEndTime.toEpochMilli()) {
                 // only trims the beginning of the slot
                 // adds the slot to the list
@@ -189,8 +288,8 @@ public class Engine {
 
                     adjustedUserFreeSlots.add(new TimeSlot(new DateTime(insOfUserStudyStartTime.toEpochMilli()), new DateTime(endOfCurrentSlot)));
                     totalFreeTime += (endOfCurrentSlot - insOfUserStudyStartTime.toEpochMilli()) / Constants.MILLIS_TO_HOUR;
-                }
-            }
+                }*/
+            /*}*/
         }
 
         return new DTOfreetime(adjustedUserFreeSlots, totalFreeTime);
