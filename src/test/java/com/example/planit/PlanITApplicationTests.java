cdf701a6049bf5bb71d5fdd86967efa4f0cee57e
kingd <kingddd301@gmail.com>
2023-04-24 18:03:05 +0300

    Add test user and more tests to unit tests

--- src/test/java/com/example/planit/PlanITApplicationTests.java
+++ src/test/java/com/example/planit/PlanITApplicationTests.java
@@ -31,6 +31,8 @@ class PlanITApplicationTests {
 
     private CalendarEngine calendarEngine;
 
+    private String subjectIDForTestInput = "112510677559500692451";
+
     @Before
     public void init() {
         String clientId = env.getProperty("spring.security.oauth2.client.registration.google.client-id");
@@ -42,13 +44,101 @@ class PlanITApplicationTests {
     }
 
     @Test
-    void authorizedUserGeneratePlan() {
+    void noExams() {
         init();
         String expectedOutput = Constants.ERROR_NO_EXAMS_FOUND;
         String actualOutput = calendarEngine.scanUserEvents(
-                "108015708438157431041",
+                subjectIDForTestInput,
                 "2022-12-31T22:00:00.000Z",
+                "2023-01-01T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void OneExam() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-01-01T22:00:00.000Z",
+                "2023-01-15T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void moreThenOneExam() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-01-15T22:00:00.000Z",
                 "2023-01-31T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void unrecognizedCourseName() {
+        init();
+        String expectedOutput = Constants.ERROR_NO_EXAMS_FOUND;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-02-01T22:00:00.000Z",
+                "2023-02-15T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void courseNameInHebrew() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-02-16T22:00:00.000Z",
+                "2023-02-27T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void courseNameInEnglish() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-03-01T22:00:00.000Z",
+                "2023-03-15T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void oneExamWithFullDayEvents() {
+        init();
+        String expectedOutput = Constants.UNHANDLED_FULL_DAY_EVENTS;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-03-16T22:00:00.000Z",
+                "2023-03-29T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void threeExamsWithTwoWeeks() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-05-01T22:00:00.000Z",
+                "2023-05-15T21:59:59.000Z").getDetails();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void twoExamsWithFourWeeks() {
+        init();
+        String expectedOutput = Constants.NO_PROBLEM;
+        String actualOutput = calendarEngine.scanUserEvents(
+                subjectIDForTestInput,
+                "2023-05-16T22:00:00.000Z",
+                "2023-06-15T21:59:59.000Z").getDetails();
         Assertions.assertEquals(expectedOutput, actualOutput);
     }
 
